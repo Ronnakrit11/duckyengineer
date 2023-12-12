cb2dfc35f6d178a4a7f7f2d650a621454d87da37
Teerapong <teera04164@gmail.com>
2023-12-12 21:22:22 +0700

    fix padding

--- app/components/Course/CourseCard.tsx
+++ app/components/Course/CourseCard.tsx
@@ -14,7 +14,7 @@ const CourseCard: FC<Props> = ({ item, isProfile }) => {
     <Link
       href={!isProfile ? `/course/${item._id}` : `course-access/${item._id}`}
     >
-      <div className="w-full min-h-[35vh] box-shadow-grow  dark:bg-slate-500 dark:bg-opacity-20 backdrop-blur  rounded-lg p-3">
+      <div className="w-full min-h-[35vh] box-shadow-grow  dark:bg-slate-500 dark:bg-opacity-20 backdrop-blur rounded-lg">
         <Image
           src={item.thumbnail.url}
           width={500}
@@ -24,40 +24,43 @@ const CourseCard: FC<Props> = ({ item, isProfile }) => {
           alt=""
         />
         <br />
-        <div className="min-h-[45px]">
-          <h1 className="font-Poppins text-[14px] font-semibold md:text-[16px] text-[#555555] dark:text-[#fff]">
-            {item.name}
-          </h1>
-        </div>
-        <div className="min-h-[50px]">
-          <p className="font-Poppins text-[12px] md:text-[14px] text-[#777] dark:text-[#fff]">
-            {item.description}
-          </p>
-        </div>
-        <div className="w-full flex flex-col md:flex-row items-center justify-between">
-          <Ratings rating={item.ratings} />
-          <h5
-            className={`text-black dark:text-[#fff] text-[10px] md:text-[12px] ${isProfile && "hidden 800px:inline"
-              }`}
-          >
-            {item.purchased} Students
-          </h5>
-        </div>
-        <div className="w-full flex flex-col-reverse md:flex-row  items-center justify-between pt-1 md:pt-3">
-          <div className="flex">
-            <h3 className="text-[#dc3545] dark:text-[#fff]">
-              {item.price === 0 ? "Free" : item.price + "฿"}
-            </h3>
-            <h5 className="pl-3 text-[14px] mt-[-5px] line-through opacity-80 text-[#777] dark:text-[#fff]">
-              {item.estimatedPrice}฿
+        <div className="p-3">
+          <div className="min-h-[45px]">
+            <h1 className="font-Poppins text-[14px] font-semibold md:text-[16px] text-[#555555] dark:text-[#fff]">
+              {item.name}
+            </h1>
+          </div>
+          <div className="min-h-[50px]">
+            <p className="font-Poppins text-[12px] md:text-[14px] text-[#777] dark:text-[#fff]">
+              {item.description}
+            </p>
+          </div>
+          <div className="w-full flex flex-col md:flex-row items-center justify-between">
+            <Ratings rating={item.ratings} />
+            <h5
+              className={`text-black dark:text-[#fff] text-[10px] md:text-[12px] ${isProfile && "hidden 800px:inline"
+                }`}
+            >
+              {item.purchased} Students
             </h5>
           </div>
-          <div className="flex items-center pb-3">
-            <h5 className=" text-black dark:text-[#777] text-[10px] md:text-[12px]">
-              {item.courseData?.length} Lectures
-            </h5>
+          <div className="w-full flex flex-col-reverse md:flex-row  items-center justify-between pt-1 md:pt-3">
+            <div className="flex">
+              <h3 className="text-[#dc3545] dark:text-[#fff]">
+                {item.price === 0 ? "Free" : item.price + "฿"}
+              </h3>
+              <h5 className="pl-3 text-[14px] mt-[-5px] line-through opacity-80 text-[#777] dark:text-[#fff]">
+                {item.estimatedPrice}฿
+              </h5>
+            </div>
+            <div className="flex items-center pb-3">
+              <h5 className=" text-black dark:text-[#777] text-[10px] md:text-[12px]">
+                {item.courseData?.length} Lectures
+              </h5>
+            </div>
           </div>
         </div>
+
       </div>
     </Link>
   );
